ade663e82dd1fad96e068aa991975568402ebaf5
nievespg1 <gabriel@nievespg.com>
2018-11-23 17:09:06 -0400

    Added a ton of ...

--- js/modal/nav_bar.js
+++ js/modal/nav_bar.js
@@ -15,8 +15,8 @@ let NAV_BAR_SINOPSIS = `
   <div class="topnav">
   	<ul>
   		<li><a href="../index.html">Contest Rules</a></li>
-  		<li><a class="active" href="./src/actores_sipnosis_criticos.html">Sipnosis</a></li>
-  		<li><a href="../src/rating.html">Critics</a></li>
+  		<li><a class="active" href="./actores_sipnosis_criticos.html">Sipnosis</a></li>
+  		<li><a href="./rating.html">Critics</a></li>
   		<li><a href="https://www.youtube.com/watch?v=XKMSP9OKspQ" target="_blank">Trailer</a></li>
   		<li><a href="https://www.instagram.com/spiderswebmovie/" target="_blank">Instagram</a></li>
   		<li><a href="../index.html">Subscription</a></li>
@@ -28,8 +28,8 @@ let NAV_BAR_RATING = `
   <div class="topnav">
   	<ul>
   		<li><a href="../index.html">Contest Rules</a></li>
-  		<li><a href="../src/actores_sipnosis_criticos.html">Sipnosis</a></li>
-  		<li><a class="active" href="./src/rating.html">Critics</a></li>
+  		<li><a href="./actores_sipnosis_criticos.html">Sipnosis</a></li>
+  		<li><a class="active" href="./rating.html">Critics</a></li>
   		<li><a href="https://www.youtube.com/watch?v=XKMSP9OKspQ" target="_blank">Trailer</a></li>
   		<li><a href="https://www.instagram.com/spiderswebmovie/" target="_blank">Instagram</a></li>
   		<li><a href="../index.html">Subscription</a></li>
