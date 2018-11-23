80fbad7e663dc6c1e09e0ce4a5e698ca9412d76d
nievespg1 <gabriel@nievespg.com>
2018-11-23 17:13:00 -0400

    Added a ton of ...

--- js/modal/nav_bar.js
+++ js/modal/nav_bar.js
@@ -49,3 +49,16 @@ let NAV_BAR_SUSCRIPTION = `
   	</ul>
   </div>
 `;
+
+let NAV_BAR_SUB_RES = `
+  <div class="topnav">
+  	<ul>
+  		<li><a href="../index.html">Contest Rules</a></li>
+  		<li><a href="./actores_sipnosis_criticos.html">Sipnosis</a></li>
+  		<li><a href="./rating.html">Critics</a></li>
+  		<li><a href="https://www.youtube.com/watch?v=XKMSP9OKspQ" target="_blank">Trailer</a></li>
+  		<li><a href="https://www.instagram.com/spiderswebmovie/" target="_blank">Instagram</a></li>
+  		<li><a class="active" href="../index.html">Subscription</a></li>
+  	</ul>
+  </div>
+`;
